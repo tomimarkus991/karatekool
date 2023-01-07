bfba29750293289b5af95ae9c22913d94358e376
tomimarkus991 <tomimarkusalber@gmail.com>
2023-01-07 18:52:14 +0200

    Remove big mobile navbar

--- src/components/elements/navbars/NavbarTopMobile.tsx
+++ src/components/elements/navbars/NavbarTopMobile.tsx
@@ -21,7 +21,7 @@ export const NavbarTopMobile = () => {
   return (
     <motion.div
       style={{
-        height: useTransform(scrollYBoundedProgress, [0, 1], [150, 60]),
+        height: useTransform(scrollYBoundedProgress, [0, 1], [60, 60]),
         backgroundColor: useMotionTemplate`rgba(255, 255, 255, ${useTransform(
           scrollYBoundedProgressThrottled,
           [0, 1],
@@ -30,7 +30,7 @@ export const NavbarTopMobile = () => {
       }}
       className="mx-auto inset-x-0 my-2 fixed top-0 z-[1200] flex rounded-md drop-shadow-lg backdrop-blur-md shadow-inner items-center w-[95%]"
     >
-      <div className="flex flex-1 items-center justify-between px-4">
+      <div className="flex items-center justify-between flex-1 px-4">
         <div className="font-semibold">
           <p className="text-3xl">nüke</p>
           <p className="text-lg">karate-do klubi</p>
@@ -41,7 +41,7 @@ export const NavbarTopMobile = () => {
               id="sidebar-button"
               role="button"
               tabIndex={0}
-              className="flex cursor-pointer flex-row items-center"
+              className="flex flex-row items-center cursor-pointer"
               onClick={() => {
                 setPrevSidebarState("closed");
                 setSidebarState("mobile");
