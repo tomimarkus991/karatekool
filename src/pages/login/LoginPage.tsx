0bdaf30098a51de327cf65fd453cee967b12910d
tomimarkus991 <tomimarkusalber@gmail.com>
2023-01-11 14:28:57 +0200

    register and login style changes

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -8,7 +8,7 @@ import { HiEyeOff, HiEye } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 import { yupSchemas } from "app-constants";
-import { LoginFormValues } from "components";
+import { DefaultPageWrapper, LoginFormValues } from "components";
 import { definedRoutes } from "routes";
 
 export const LoginPage = () => {
@@ -19,82 +19,84 @@ export const LoginPage = () => {
     password: "",
   });
   return (
-    <div className="flex w-full m-auto items-center justify-center h-[70vh] max-w-md">
-      <div className="">
-        <Formik
-          initialValues={initialValues}
-          validationSchema={yupSchemas.LoginYupSchema}
-          validateOnChange={true}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            setSubmitting(true);
+    <DefaultPageWrapper>
+      <div className="max-w-[25rem] m-auto">
+        <div className="p-6 bg-white rounded-xl">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={yupSchemas.LoginYupSchema}
+            validateOnChange={true}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setSubmitting(true);
 
-            //   const { email, name, password } = values;
+              //   const { email, name, password } = values;
 
-            resetForm();
+              resetForm();
 
-            setSubmitting(false);
-          }}
-        >
-          {({ isValid }) => {
-            return (
-              <Form className={clsx("flex flex-col")}>
-                <div className="flex flex-row items-center justify-between pt-6 px-7">
-                  <p className="text-xl font-bold">Logi sisse</p>
-                </div>
-                <div
-                  className={clsx(
-                    "scrollbar-hide",
-                    "flex overflow-y-auto items-center flex-col py-2 px-3 h-[10vh] min-h-[10rem]"
-                  )}
-                >
-                  <div className="w-full m-auto mt-3 space-y-2">
-                    <FormikInput className="w-full" placeholder="Email" name="email" />
-                    <FormikInput
-                      className="w-full"
-                      placeholder="Salasõna"
-                      name="password"
-                      type={isPasswordVisible ? "text" : "password"}
-                      inputAfterfix={
-                        <div
-                          className="cursor-pointer"
-                          onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                        >
-                          {isPasswordVisible ? (
-                            <HiEyeOff className="w-5 h-5 fill-stone-600" />
-                          ) : (
-                            <HiEye className="w-5 h-5 fill-stone-600" />
-                          )}
-                        </div>
-                      }
-                    />
-                    <Link to={definedRoutes.forgotPassword}>
-                      <p className="mt-2 text-sm font-semibold text-center cursor-pointer text-secondary">
-                        Unustasid parooli?
+              setSubmitting(false);
+            }}
+          >
+            {({ isValid }) => {
+              return (
+                <Form className={clsx("flex flex-col")}>
+                  <div className="pl-3">
+                    <p className="text-xl font-bold">Logi sisse</p>
+                  </div>
+                  <div
+                    className={clsx(
+                      "scrollbar-hide",
+                      "flex overflow-y-auto items-center flex-col py-2 px-3 h-[10vh] min-h-[10rem]"
+                    )}
+                  >
+                    <div className="w-full m-auto mt-3 space-y-2">
+                      <FormikInput className="w-full" placeholder="Email" name="email" />
+                      <FormikInput
+                        className="w-full"
+                        placeholder="Salasõna"
+                        name="password"
+                        type={isPasswordVisible ? "text" : "password"}
+                        inputAfterfix={
+                          <div
+                            className="cursor-pointer"
+                            onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                          >
+                            {isPasswordVisible ? (
+                              <HiEyeOff className="w-5 h-5 fill-stone-600" />
+                            ) : (
+                              <HiEye className="w-5 h-5 fill-stone-600" />
+                            )}
+                          </div>
+                        }
+                      />
+                      <Link to={definedRoutes.forgotPassword}>
+                        <p className="mt-2 text-sm font-semibold text-center cursor-pointer text-secondary">
+                          Unustasid parooli?
+                        </p>
+                      </Link>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col items-center justify-center">
+                    <Button
+                      variant="red"
+                      className="w-[20rem] text-xl bg-primary"
+                      type="submit"
+                      isValid={isValid}
+                    >
+                      Logi sisse
+                    </Button>
+                    <Link to={definedRoutes.register}>
+                      <p className="mt-2 text-sm font-semibold cursor-pointer text-secondary">
+                        Mul pole veel kasutajat. Registreeri
                       </p>
                     </Link>
                   </div>
-                </div>
-
-                <div className="flex flex-col items-center justify-center">
-                  <Button
-                    variant="red"
-                    className="w-[20rem] text-xl bg-primary"
-                    type="submit"
-                    isValid={isValid}
-                  >
-                    Logi sisse
-                  </Button>
-                  <Link to={definedRoutes.register}>
-                    <p className="mt-2 text-sm font-semibold cursor-pointer text-secondary">
-                      Mul pole veel kasutajat. Registreeri
-                    </p>
-                  </Link>
-                </div>
-              </Form>
-            );
-          }}
-        </Formik>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
       </div>
-    </div>
+    </DefaultPageWrapper>
   );
 };
--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -8,6 +8,7 @@ import { HiEyeOff, HiEye } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 import { yupSchemas } from "app-constants";
+import { DefaultPageWrapper } from "components";
 import { definedRoutes } from "routes";
 
 export interface RegisterFormValues {
@@ -29,99 +30,101 @@ export const RegisterPage = () => {
   });
 
   return (
-    <div className="flex w-full m-auto items-center justify-center h-[70vh] max-w-md">
-      <div className="">
-        <Formik
-          initialValues={initialValues}
-          validationSchema={yupSchemas.LoginYupSchema}
-          validateOnChange={true}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            setSubmitting(true);
+    <DefaultPageWrapper>
+      <div className="max-w-[25rem] m-auto">
+        <div className="p-6 bg-white rounded-xl">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={yupSchemas.LoginYupSchema}
+            validateOnChange={true}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              setSubmitting(true);
 
-            //   const { email, name, password } = values;
+              //   const { email, name, password } = values;
 
-            resetForm();
+              resetForm();
 
-            setSubmitting(false);
-          }}
-        >
-          {({ isValid }) => {
-            return (
-              <Form className={clsx("flex flex-col")}>
-                <div className="flex flex-row items-center justify-between pt-6 px-7">
-                  <p className="text-xl font-bold">Registreeri</p>
-                </div>
-                <div
-                  className={clsx(
-                    "scrollbar-hide",
-                    "flex overflow-y-auto items-center flex-col py-2 px-3 h-[27vh] min-h-[15rem]"
-                  )}
-                >
-                  <p className="mt-2 text-sm font-semibold cursor-pointer text-secondary">
-                    Kui sa pole veel luba taotlenud, vajuta siia
-                  </p>
-                  <div className="w-full mt-3 space-y-2">
-                    <FormikInput className="w-full" placeholder="Nimi" name="name" />
-                    <FormikInput className="w-full" placeholder="Email" name="email" />
-                    <FormikInput
-                      className="w-full"
-                      placeholder="Salasõna"
-                      name="password"
-                      type={isPasswordVisible ? "text" : "password"}
-                      inputAfterfix={
-                        <div
-                          className="cursor-pointer"
-                          onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                        >
-                          {isPasswordVisible ? (
-                            <HiEyeOff className="w-5 h-5 fill-stone-600" />
-                          ) : (
-                            <HiEye className="w-5 h-5 fill-stone-600" />
-                          )}
-                        </div>
-                      }
-                    />
-                    <FormikInput
-                      className="w-full"
-                      placeholder="Korda salasõna"
-                      name="passwordConfirmation"
-                      type={isConfirmPasswordVisible ? "text" : "password"}
-                      inputAfterfix={
-                        <div
-                          className="cursor-pointer"
-                          onClick={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)}
-                        >
-                          {isConfirmPasswordVisible ? (
-                            <HiEyeOff className="w-5 h-5 fill-stone-600" />
-                          ) : (
-                            <HiEye className="w-5 h-5 fill-stone-600" />
-                          )}
-                        </div>
-                      }
-                    />
+              setSubmitting(false);
+            }}
+          >
+            {({ isValid }) => {
+              return (
+                <Form className={clsx("flex flex-col")}>
+                  <div className="flex flex-row items-center justify-between pl-3">
+                    <p className="text-xl font-bold">Registreeri</p>
                   </div>
-                </div>
-
-                <div className="flex flex-col items-center justify-center">
-                  <Button
-                    variant="red"
-                    className="w-[20rem] text-xl bg-primary"
-                    type="submit"
-                    isValid={isValid}
+                  <div
+                    className={clsx(
+                      "scrollbar-hide",
+                      "flex overflow-y-auto items-center flex-col py-2 px-3 h-[27vh] min-h-[15rem]"
+                    )}
                   >
-                    Registreeri
-                  </Button>
-                  <Link to={definedRoutes.login}>
                     <p className="mt-2 text-sm font-semibold cursor-pointer text-secondary">
-                      Mul juba on kasutaja. Logi sisse
+                      Kui sa pole veel luba taotlenud, vajuta siia
                     </p>
-                  </Link>
-                </div>
-              </Form>
-            );
-          }}
-        </Formik>
+                    <div className="w-full mt-3 space-y-2">
+                      <FormikInput className="w-full" placeholder="Nimi" name="name" />
+                      <FormikInput className="w-full" placeholder="Email" name="email" />
+                      <FormikInput
+                        className="w-full"
+                        placeholder="Salasõna"
+                        name="password"
+                        type={isPasswordVisible ? "text" : "password"}
+                        inputAfterfix={
+                          <div
+                            className="cursor-pointer"
+                            onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                          >
+                            {isPasswordVisible ? (
+                              <HiEyeOff className="w-5 h-5 fill-stone-600" />
+                            ) : (
+                              <HiEye className="w-5 h-5 fill-stone-600" />
+                            )}
+                          </div>
+                        }
+                      />
+                      <FormikInput
+                        className="w-full"
+                        placeholder="Korda salasõna"
+                        name="passwordConfirmation"
+                        type={isConfirmPasswordVisible ? "text" : "password"}
+                        inputAfterfix={
+                          <div
+                            className="cursor-pointer"
+                            onClick={() => setIsConfirmPasswordVisible(!isConfirmPasswordVisible)}
+                          >
+                            {isConfirmPasswordVisible ? (
+                              <HiEyeOff className="w-5 h-5 fill-stone-600" />
+                            ) : (
+                              <HiEye className="w-5 h-5 fill-stone-600" />
+                            )}
+                          </div>
+                        }
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col items-center justify-center">
+                    <Button
+                      variant="red"
+                      className="w-[20rem] text-xl bg-primary"
+                      type="submit"
+                      isValid={isValid}
+                    >
+                      Registreeri
+                    </Button>
+                    <Link to={definedRoutes.login}>
+                      <p className="mt-2 text-sm font-semibold cursor-pointer text-secondary">
+                        Mul juba on kasutaja. Logi sisse
+                      </p>
+                    </Link>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
       </div>
-    </div>
+    </DefaultPageWrapper>
   );
 };
