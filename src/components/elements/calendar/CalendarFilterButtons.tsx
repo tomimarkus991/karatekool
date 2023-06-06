ad047c02dffa582d41db0a1fbb6ddd42d5b7ff7e
tomimarkus991 <tomimarkusalber@gmail.com>
2023-06-06 22:24:33 +0300

    Add new calendar views and buttons

--- src/components/elements/calendar/CalendarFilterButtons.tsx
+++ src/components/elements/calendar/CalendarFilterButtons.tsx
@@ -1,8 +1,11 @@
 import { HiDownload } from "react-icons/hi";
+import { TbBoxMultiple8 } from "react-icons/tb";
 
 import { RealButton } from "@/components";
 import { useCalendarFilters } from "@/context";
 import { buttonVariantMapper, groupLetters } from "@/lib";
+
+import { useUser } from "../../../hooks";
 
 interface Props {
   downloadScreenshot: () => void;
@@ -10,13 +13,13 @@ interface Props {
 
 export const CalendarFilterButtons = ({ downloadScreenshot }: Props) => {
   const { setLetter } = useCalendarFilters();
+  const { data: user } = useUser();
   return (
-    <div className="grid justify-center grid-cols-4 gap-2 max-w-[340px] m-auto xs2:flex xs2:justify-start xs2:max-w-none xs2:m-0 xs2:ml-4">
+    <div className="grid justify-center grid-cols-5 gap-2 max-w-[14rem] m-auto sm:flex sm:justify-start sm:max-w-none sm:m-0 sm:ml-4">
       {groupLetters.map(letter => {
         return (
           <RealButton
             key={letter}
-            className="active:translate-y-[0.2rem] active:duration-75"
             variant={buttonVariantMapper(letter)}
             size="oneLetter"
             focus={true}
@@ -26,9 +29,19 @@ export const CalendarFilterButtons = ({ downloadScreenshot }: Props) => {
           </RealButton>
         );
       })}
-      <RealButton size="icon" onClick={downloadScreenshot}>
-        <HiDownload className="w-5 h-5 text-white" />
+      <RealButton size="icon" className="px-0 sm:px-3" onClick={downloadScreenshot}>
+        <HiDownload className="w-6 h-6 text-white" />
       </RealButton>
+      {user && (
+        <RealButton
+          onClick={() => setLetter("all")}
+          size="icon"
+          className="px-0 sm:px-3"
+          variant="light"
+        >
+          <TbBoxMultiple8 className="w-6 h-6 text-stone-800" />
+        </RealButton>
+      )}
       {/* <RealButton size="sm" onClick={() => setLetter("all")}>
         All
       </RealButton>
