14595e6d2c371a3b22e928103ab8997e299b9ef0
tomimarkus991 <tomimarkusalber@gmail.com>
2023-06-18 16:18:27 +0300

    Add feedback when sending

--- src/app/apply-to-club/page.tsx
+++ src/app/apply-to-club/page.tsx
@@ -2,6 +2,7 @@
 
 import clsx from "clsx";
 import { Formik, Form } from "formik";
+import { AnimatePresence, MotionConfig, motion } from "framer-motion";
 import { CheckCircle2 } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
@@ -24,6 +25,8 @@ interface FormValues {
   reason: string;
 }
 
+const transition = { type: "ease", ease: "easeInOut", duration: 1 };
+
 export default function Page() {
   const [initialValues] = useState<FormValues>({
     name: "",
@@ -35,127 +38,161 @@ export default function Page() {
   const { data: whitelistedEmails } = useGetEmailWhitelist();
 
   const [isEmailWhitelisted, setIsEmailWhitelisted] = useState(false);
+  const [requestSuccess, setRequestSuccess] = useState(false);
 
   return (
-    <div className="max-w-[25rem] m-auto p-6 bg-white rounded-xl overflow-hidden">
-      <ResizablePanel duration={1}>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={YupSchemas.ApplyToClub}
-          validateOnChange={true}
-          onSubmit={async (formData, { setSubmitting }) => {
-            setSubmitting(true);
+    <MotionConfig transition={transition}>
+      <div className="max-w-[25rem] m-auto p-6 bg-white rounded-xl overflow-hidden relative">
+        <ResizablePanel duration={1}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={YupSchemas.ApplyToClub}
+            validateOnChange={true}
+            onSubmit={async (formData, { setSubmitting }) => {
+              setSubmitting(true);
 
-            for (const whitelistedEmail of whitelistedEmails || []) {
-              if (whitelistedEmail.email === formData.email) {
-                setIsEmailWhitelisted(true);
+              for (const whitelistedEmail of whitelistedEmails || []) {
+                if (whitelistedEmail.email === formData.email) {
+                  setIsEmailWhitelisted(true);
+                }
               }
-            }
 
-            const data1 = await fetch("/api/send-application", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(formData),
-            });
-            console.log(data1);
-
-            setSubmitting(false);
-          }}
-        >
-          {({ isValid, handleSubmit, values, resetForm }) => {
-            for (const whitelistedEmail of whitelistedEmails || []) {
-              if (whitelistedEmail.email === values.email) {
-                setIsEmailWhitelisted(true);
-                resetForm();
+              const applicationSent = await fetch("/api/send-application", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+              });
+              if (applicationSent.ok) {
+                setRequestSuccess(true);
               }
-            }
 
-            return (
-              <>
-                {isEmailWhitelisted ? (
-                  <div className="flex flex-col items-center justify-center">
-                    <CheckCircle2 size={42} className="mb-3 text-green-600" />
-                    <div className="flex items-center justify-center">
-                      <p className="text-lg font-semibold">
-                        Sul on juba luba olemas, et registreerida
-                      </p>
-                    </div>
-                    <Link href="/register">
-                      <AnimationWrapper variants={animations.smallScaleXs}>
-                        <p className="mt-4 text-lg font-semibold cursor-pointer text-secondary">
-                          Registreeri
-                        </p>
-                      </AnimationWrapper>
-                    </Link>
+              setSubmitting(false);
+            }}
+          >
+            {({ isValid, handleSubmit, values, resetForm }) => {
+              for (const whitelistedEmail of whitelistedEmails || []) {
+                if (whitelistedEmail.email === values.email) {
+                  setIsEmailWhitelisted(true);
+                  resetForm();
+                }
+              }
 
-                    <AnimationWrapper
-                      variants={animations.smallScaleXs}
-                      onClick={() => {
-                        setIsEmailWhitelisted(false);
-                      }}
-                    >
-                      <p className="mt-4 font-semibold cursor-pointer text-secondary">
-                        Taotle ikka
-                      </p>
-                    </AnimationWrapper>
-                  </div>
-                ) : (
-                  <Form className={clsx("flex flex-col")}>
-                    <div className="flex flex-row items-center justify-between pl-3">
-                      <p className="text-xl font-bold">Taotle luba</p>
-                    </div>
-                    <div className={clsx("flex items-center flex-col py-2 mb-5 px-3")}>
-                      <div className="w-full mt-3 space-y-2">
-                        <FormikInput
-                          required
-                          className="w-full"
-                          label="Email"
-                          placeholder="Email"
-                          name="email"
-                        />
-                        <FormikInput
-                          required
-                          className="w-full"
-                          label="Nimi"
-                          placeholder="Nimi"
-                          name="name"
-                        />
-                        <FormikInput
-                          required
-                          className="w-full"
-                          label="Grupp"
-                          placeholder="Grupp"
-                          name="group"
-                        />
-                        <FormikInput
-                          required
-                          className="w-full"
-                          label="Miks taotled?"
-                          placeholder="Miks taotled?"
-                          name="reason"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="flex flex-col items-center justify-center">
-                      <RealButton
-                        variant="red"
-                        type="submit"
-                        onClick={handleSubmit as any}
-                        isValid={isValid}
+              return (
+                <>
+                  <AnimatePresence mode="popLayout">
+                    {!requestSuccess ? (
+                      <motion.div
+                        exit={{ opacity: 0 }}
+                        transition={{
+                          ...transition,
+                          duration: transition.duration / 1.5,
+                        }}
+                        key="form"
                       >
-                        Saada taotlus
-                      </RealButton>
-                    </div>
-                  </Form>
-                )}
-              </>
-            );
-          }}
-        </Formik>
-      </ResizablePanel>
-    </div>
+                        {isEmailWhitelisted ? (
+                          <div className="flex flex-col items-center justify-center">
+                            <CheckCircle2 size={42} className="mb-3 text-green-600" />
+                            <div className="flex items-center justify-center">
+                              <p className="text-lg font-semibold">
+                                Sul on juba luba olemas, et registreerida
+                              </p>
+                            </div>
+                            <Link href="/register">
+                              <AnimationWrapper variants={animations.smallScaleXs}>
+                                <p className="mt-4 text-lg font-semibold cursor-pointer text-secondary">
+                                  Registreeri
+                                </p>
+                              </AnimationWrapper>
+                            </Link>
+
+                            <AnimationWrapper
+                              variants={animations.smallScaleXs}
+                              onClick={() => {
+                                setIsEmailWhitelisted(false);
+                              }}
+                            >
+                              <p className="mt-4 font-semibold cursor-pointer text-secondary">
+                                Taotle ikka
+                              </p>
+                            </AnimationWrapper>
+                          </div>
+                        ) : (
+                          <Form className={clsx("flex flex-col")}>
+                            <div className="flex flex-row items-center justify-between pl-3">
+                              <p className="text-xl font-bold">Taotle luba</p>
+                            </div>
+                            <div className={clsx("flex items-center flex-col py-2 mb-5 px-3")}>
+                              <div className="w-full mt-3 space-y-2">
+                                <FormikInput
+                                  required
+                                  className="w-full"
+                                  label="Email"
+                                  placeholder="Email"
+                                  name="email"
+                                />
+                                <FormikInput
+                                  required
+                                  className="w-full"
+                                  label="Nimi"
+                                  placeholder="Nimi"
+                                  name="name"
+                                />
+                                <FormikInput
+                                  required
+                                  className="w-full"
+                                  label="Grupp"
+                                  placeholder="Grupp"
+                                  name="group"
+                                />
+                                <FormikInput
+                                  required
+                                  className="w-full"
+                                  label="Miks taotled?"
+                                  placeholder="Miks taotled?"
+                                  name="reason"
+                                />
+                              </div>
+                            </div>
+
+                            <div className="flex flex-col items-center justify-center">
+                              <RealButton
+                                variant="red"
+                                type="submit"
+                                onClick={handleSubmit as any}
+                                isValid={isValid}
+                              >
+                                Saada taotlus
+                              </RealButton>
+                            </div>
+                          </Form>
+                        )}
+                      </motion.div>
+                    ) : (
+                      <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{
+                          ...transition,
+                          duration: transition.duration,
+                          delay: transition.duration,
+                        }}
+                        key="success"
+                      >
+                        <div className="flex flex-col items-center justify-center">
+                          <CheckCircle2 size={42} className="mb-3 text-green-600" />
+                          <p className="text-lg font-semibold">Taotlus saadetud!</p>
+                        </div>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </>
+              );
+            }}
+          </Formik>
+        </ResizablePanel>
+      </div>
+    </MotionConfig>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 
-import { LetterDecryptor } from "../components/elements/LetterDecryptor";
+// import { LetterDecryptor } from "../components/elements/LetterDecryptor";
 
 // import { Footer } from "@/components/elements/navigation/Footer";
 // import { NavbarTop } from "@/components/elements/navigation/NavbarTop";
@@ -16,7 +16,7 @@ export default function Page() {
   return (
     <div>
       <p className="text-3xl font-semibold text-center">
-        <LetterDecryptor>Coming soon</LetterDecryptor>
+        {/* <LetterDecryptor>Coming soon</LetterDecryptor> */}
       </p>
     </div>
   );
