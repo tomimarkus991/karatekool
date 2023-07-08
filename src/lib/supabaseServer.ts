d7b933817666ac331102a92362dfd60494db9d81
Tomi Markus Alber <tomimarkus.alber@egrupp.ee>
2023-07-08 18:13:27 +0300

    Update supabase and next

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,5 +1,5 @@
-import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { headers, cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 import { Database } from "../types/supabase";
 
@@ -7,9 +7,13 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const createServerClient = () =>
-  createServerComponentSupabaseClient<Database>({
-    headers,
-    cookies,
-    supabaseKey,
-    supabaseUrl,
-  });
+  createServerComponentClient<Database>(
+    {
+      cookies,
+    },
+    {
+      // headers,
+      supabaseKey,
+      supabaseUrl,
+    }
+  );
