22433d6d2c677a4765c920f520d27fca2d88b1ba
tomimarkus991 <tomimarkusalber@gmail.com>
2023-07-10 22:19:05 +0300

    Try to fix pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,14 +22,14 @@ import { Three3DLayout } from "@/three/components/Three3DLayout";
 
 const APP_NAME = "Karatekool Nüke";
 const APP_DEFAULT_TITLE = "Karatekool Nüke";
-// const APP_TITLE_TEMPLATE = "%s - Karatekool Nüke";
+const APP_TITLE_TEMPLATE = "%s - Karatekool Nüke";
 const APP_DESCRIPTION = "Karatekool nüke koduleht!";
 
 export const metadata: Metadata = {
   applicationName: APP_NAME,
   title: {
     default: APP_DEFAULT_TITLE,
-    // template: APP_TITLE_TEMPLATE,
+    template: APP_TITLE_TEMPLATE,
   },
   description: APP_DESCRIPTION,
   manifest: "/manifest.json",
@@ -77,12 +77,11 @@ export default async function RootLayout({ children }: NextLayoutProps) {
       suppressHydrationWarning
     >
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
-        {/* <link rel="manifest" href="/icons/site.webmanifest"/> */}
+        <link rel="manifest" href="/manifest.json"/>
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5"/>
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#4d4f52"/>
