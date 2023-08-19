ca66189d8c585017c1270f0df00c7b6b37396b7f
tomimarkus991 <tomimarkusalber@gmail.com>
2023-08-19 08:32:01 +0300

    Improive seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { Metadata } from "next";
 import { MainComponent } from "./(home_components)/HomepageComponents";
 
 export const metadata: Metadata = {
-  title: "Nüke Traditsiooniline Karateklubi",
+  title: "Nüke Karatekool",
   description:
-    "Nüke on Eesti kõige pikema traditsiooniga karateklubi. Lahedad treeningud Tallinn, Vanalinn. Karate-do väärtused, isikupõhine lähenemine, pühendunud treenerid ja palju muud.",
+    "Nüke on Eesti kõige pikema traditsiooniga karateklubi. Lahedad treeningud Eestis Tallinn, Vanalinn. Karate-do väärtused, isikupõhine lähenemine, pühendunud treenerid ja palju muud.",
 };
 
 export default function Page() {
