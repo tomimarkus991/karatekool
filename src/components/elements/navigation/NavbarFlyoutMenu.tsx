d08d646d5140a018196feff274fb302343be9e94
tomimarkus991 <tomimarkusalber@gmail.com>
2023-09-11 21:17:43 +0300

    Add correct routes and pages

--- src/components/elements/navigation/NavbarFlyoutMenu.tsx
+++ src/components/elements/navigation/NavbarFlyoutMenu.tsx
@@ -1,48 +1,57 @@
 "use client";
 
 import { Popover, Transition } from "@headlessui/react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Fragment, useRef } from "react";
-import { HiChartPie, HiChevronDown } from "react-icons/hi";
+import {
+  HiChevronDown,
+  HiFilm,
+  HiFire,
+  HiMap,
+  HiPaperClip,
+  HiPhotograph,
+  HiUserGroup,
+} from "react-icons/hi";
 
 import { cn } from "@/lib";
 
-const solutions = [
+const routes = [
   {
     name: "Klubist",
-    description: "Get a better understanding of your traffic",
+    description: "Rohkem meie klubist",
     href: "/klubist",
-    icon: HiChartPie,
+    icon: HiUserGroup,
   },
   {
     name: "Karate-do",
-    description: "Speak directly to your customers",
+    description: "Rohkem karate-do kunstist",
     href: "/karate-do",
-    icon: HiChartPie,
+    icon: HiFire,
   },
   {
     name: "Videod",
-    description: "Your customers' data will be safe and secure",
+    description: "Videod meie trennidest",
     href: "/videod",
-    icon: HiChartPie,
+    icon: HiFilm,
   },
   {
     name: "Pildid",
-    description: "Connect with third-party tools",
+    description: "Vaata karateklubi Nüke pilte",
     href: "/pildid",
-    icon: HiChartPie,
+    icon: HiPhotograph,
   },
   {
     name: "Buklet",
-    description: "Build strategic funnels that will convert",
+    description: "Vaata meie lahedat Bukletti",
     href: "/buklet",
-    icon: HiChartPie,
+    icon: HiMap,
   },
   {
     name: "Viimased sündmused",
-    description: "Build strategic funnels that will convert",
-    href: "/viimased-sündmused",
-    icon: HiChartPie,
+    description: "Millega meie karatekad tegelevad",
+    href: "/last-events",
+    icon: HiPaperClip,
   },
 ];
 interface Props {
@@ -71,8 +80,6 @@ export const NavbarFlyoutMenu = ({ href }: Props) => {
   return (
     <Popover className="relative">
       {({ open }) => {
-        console.log(open);
-
         return (
           <>
             <div onMouseEnter={() => handleEnter(open)} onMouseLeave={() => handleLeave(open)}>
@@ -82,14 +89,21 @@ export const NavbarFlyoutMenu = ({ href }: Props) => {
                   pathname === href ? "text-primary" : "text-text-primary",
                   "relative flex group",
                   "lg:text-lg items-center py-3 font-semibold",
-                  "transition ease-in-out duration-200 delay-150",
-                  "group-hover:-translate-y-1 group-hover:scale-110 group-hover:text-primary group-hover:delay-[0ms]",
+                  "transition ease-in-out duration-200",
+                  "group-hover:-translate-y-1 group-hover:scale-110 group-hover:text-primary",
                   "group-active:translate-y-0 group-active:scale-100",
                   "focus:outline-none focus-visible:ring-0 focus-visible:ring-transparent",
                 )}
               >
                 Klubist
-                <HiChevronDown className="w-5 h-5" aria-hidden="true" />
+                <HiChevronDown
+                  className={cn(
+                    "w-5 h-5",
+                    "ease-in-out transition duration-200",
+                    open && "rotate-180 text-primary",
+                  )}
+                  aria-hidden="true"
+                />
               </Popover.Button>
 
               <Transition
@@ -103,9 +117,8 @@ export const NavbarFlyoutMenu = ({ href }: Props) => {
               >
                 <Popover.Panel className="absolute z-10 flex w-screen px-4 mt-5 -translate-x-1/2 left-1/2 max-w-max">
                   <div className="flex-auto w-screen max-w-md overflow-hidden text-sm leading-6 bg-white shadow-lg rounded-3xl ring-1 ring-gray-900/5 lg:max-w-3xl">
-                    {/* <AnimationWrapper variants={animations.navigationMenu} custom={open ? 1 : -1}> */}
                     <div className="grid grid-cols-1 p-4 gap-x-6 gap-y-1 lg:grid-cols-2">
-                      {solutions.map(item => (
+                      {routes.map(item => (
                         <div
                           key={item.name}
                           className="relative flex p-4 rounded-lg group gap-x-6 hover:bg-gray-50"
@@ -117,26 +130,15 @@ export const NavbarFlyoutMenu = ({ href }: Props) => {
                             />
                           </div>
                           <div>
-                            <a href={item.href} className="font-semibold text-gray-900">
+                            <Link href={item.href as any} className="font-semibold text-gray-900">
                               {item.name}
                               <span className="absolute inset-0" />
-                            </a>
+                            </Link>
                             <p className="mt-1 text-gray-600">{item.description}</p>
                           </div>
                         </div>
                       ))}
                     </div>
-                    <div className="px-8 py-6 bg-gray-50">
-                      <div className="flex items-center gap-x-3">
-                        <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                          Registreerimise info
-                        </h3>
-                      </div>
-                      <p className="mt-2 text-sm leading-6 text-gray-600">
-                        Empower your entire team with even more advanced tools.
-                      </p>
-                    </div>
-                    {/* </AnimationWrapper> */}
                   </div>
                 </Popover.Panel>
               </Transition>
