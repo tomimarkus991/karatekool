a42d4bf39400f9069a0f085e5d2aead29d1e6bd1
tomimarkus991 <tomimarkusalber@gmail.com>
2025-08-26 09:11:11 +0300

    Add 2025 metadata

--- src/app/(children)/registreerimise-info/page.tsx
+++ src/app/(children)/registreerimise-info/page.tsx
@@ -4,18 +4,17 @@ import { RegistreerimiseInfoPageComponent } from "./RegistreerimiseInfoPageCompo
 
 export const metadata: Metadata = {
   title: "Registreerimise info",
-  description: `Treeningud ALGKLASSIDE LASTELE, VHK spordisaal (Vene 22)
-    L-grupp
+  description: `
+Treeningud ALGKLASSIDE LASTELE, VHK spordisaal (Vene 22)
+V-grupp
 TEISIPÄEVAL kell 15:00 – 16:00
-KOLMAPÄEVAL kell 15:00 – 16:00
+KOLMAPÄEVAL kell 16:30 – 17:30
 REEDEL kell 15:00 – 16:00
 
-Treeningud noortele ja täiskasvanutele, VHK spordisaal (Vene 22)
-
-C-grupp
-TEISIPÄEVAL kell 17:45 – 19:00
-NELJAPÄEVAL kell 17:45 – 19:00
-LAUPÄEVAL (vastavalt treeninggraafikule)
+Treeningud NOORTELE ja TÄISKASVANUTELE, VHK spordisaal (Vene 22)
+S-grupp
+ESMASPÄEVAL kell 18:45 – 20:00
+KOLMAPÄEVAL kell 18:45 – 20:00
 `,
 };
 
