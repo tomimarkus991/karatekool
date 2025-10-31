b65e8ec1fdf76d29131ebedcd0839db8b6dc84bf
tomimarkus991 <tomimarkusalber@gmail.com>
2025-10-31 10:20:09 +0200

    Add new videos

--- src/app/(children)/videod/page.tsx
+++ src/app/(children)/videod/page.tsx
@@ -4,24 +4,50 @@ import ReactPlayer from "react-player";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center mt-10">
-      <p className="mb-3 font-sans text-lg text-center md:text-3xl">Karated tutvustav video</p>
-      <ReactPlayer
-        width="100%"
-        style={{ maxWidth: 800 }}
-        height={400}
-        url="https://www.youtube.com/watch?v=ExANa6rCU38"
-        controls
-      />
-      <div className="mt-10">
-        <p className="mb-3 font-sans text-lg text-center md:text-3xl">Karateklubi Nüke 25-juubel</p>
+    <div className="flex flex-row items-center justify-center mt-10">
+      <div className="flex flex-col items-center justify-center mr-20">
+        <p className="mb-3 font-sans text-lg text-center md:text-3xl">Karated tutvustav video</p>
         <ReactPlayer
           width="100%"
           style={{ maxWidth: 800 }}
           height={400}
-          url="https://www.youtube.com/watch?v=ibuDZAIPOU8"
+          url="https://www.youtube.com/watch?v=ExANa6rCU38"
           controls
         />
+        <div className="mt-10">
+          <p className="mb-3 font-sans text-lg text-center md:text-3xl">
+            Karateklubi Nüke 25-juubel
+          </p>
+          <ReactPlayer
+            width="100%"
+            style={{ maxWidth: 800 }}
+            height={400}
+            url="https://www.youtube.com/watch?v=ibuDZAIPOU8"
+            controls
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <p className="mb-3 font-sans text-lg text-center md:text-3xl">Nüke Karate</p>
+        <ReactPlayer
+          width="100%"
+          style={{ maxWidth: 800 }}
+          height={400}
+          url="https://youtu.be/dcwxf5254wQ"
+          controls
+        />
+        <div className="mt-10">
+          <p className="mb-3 font-sans text-lg text-center md:text-3xl">
+            Karatelaager Aravetel 1990 aasta suvel
+          </p>
+          <ReactPlayer
+            width="100%"
+            style={{ maxWidth: 800 }}
+            height={400}
+            url="https://youtu.be/oFMdnoZ8aSk"
+            controls
+          />
+        </div>
       </div>
     </div>
   );
